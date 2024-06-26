42de60565540fd8664fef7c9ed9136deb89bb25d
Tom Macfie <tm@paceteq.com>
2024-06-26 10:10:38 +0200

    fest: styling for deck card
    Signed-off-by: Tom Macfie <tm@paceteq.com>

--- client/src/components/ShowDecks.tsx
+++ client/src/components/ShowDecks.tsx
@@ -14,23 +14,39 @@ export const ShowDecks = () => {
     <div>
       <div className='h-[10vh] flex items-center justify-center'>Decks</div>
       <hr />
-      <ul>
+      <ul className='gap-2'>
         {decks?.map((deck) => {
           return (
-            <li>
-              <div>{deck.name}</div>
-              <input
-                type='checkbox'
-                checked={deck?.reversed}
-                onChange={async () =>
-                  await updateDeck({
-                    variables: {
-                      id: deck._id,
-                      record: { reversed: !deck.reversed },
-                    },
-                  })
-                }
-              />
+            <li className='flex justify-center gap-2 p-2 my-2 border rounded-md'>
+              <div className='flex justify-center items-center'>
+                {deck.name}
+              </div>
+              <div className='flex gap-1'>
+                <input
+                  name='reversed'
+                  id='reversed'
+                  type='checkbox'
+                  checked={deck?.reversed}
+                  onChange={async () =>
+                    await updateDeck({
+                      variables: {
+                        id: deck._id,
+                        record: { reversed: !deck.reversed },
+                      },
+                    })
+                  }
+                />
+                <label
+                  htmlFor='reversed'
+                  className='flex justify-center items-center'
+                >
+                  reversed
+                </label>
+              </div>
+              <div className='ml-auto flex flex-col'>
+                <div>view</div>
+                <div>edit</div>
+              </div>
             </li>
           );
         })}
